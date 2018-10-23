5cea6e23cb30fcbbcf9caeb5b4e6f9cbc518d3d3
Selcan Ipek-Ugay <selcan.ipek-ugay@charite.de>
2018-10-23 15:35:32 +0200

    Projekt-Struktur erweitert/angepasst

--- bin/executeTestMigrations.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd `dirname $0`/..
-cd application/modelsDoctrine
-APPLICATION_ENV=regtest ./executeMigrations.sh -n $1 $2
---
+++ src/DatenImport/Infrastructure/Persistence/CSVImportService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 23.10.18
+ * Time: 15:24
+ */
+
+namespace App\DatenImport\Infrastructure\Persistence;
+
+class CSVImportService
+{
+
+}
