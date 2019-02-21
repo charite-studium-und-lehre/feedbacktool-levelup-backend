657f52d7eda4185c2a0ab8e8c4e541b2348f87e4
Martin Dittmar <martin.dittmar@charite.de>
2019-02-21 17:33:43 +0100

    Weiter Wertungs-Persistenz

---
+++ src/Wertung/Infrastructure/Persistence/DB/DoctrineTypes/SkalaType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Wertung\Infrastructure\Persistence\DB\DoctrineTypes;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use Wertung\Domain\Skala\ProzentSkala;
+use Wertung\Domain\Skala\PunktSkala;
+use Wertung\Domain\Skala\Skala;
+use Wertung\Domain\Wertung\Punktzahl;
+
+class SkalaType extends Type
+{
+
+    const TYPE_NAME = 'wertungsSkala'; // modify to match your type name
+
+    const PROZENT_SKALA = 10;
+    const PUNKT_SKALA = 20;
+
+    const AMOUNT_FACTOR = 100;
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform) {
+        return "INTEGER";
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform) {
+        if (!$value) {
+            return null;
+        }
+        $type = $value % self::AMOUNT_FACTOR;
+        $amount = $value / self::AMOUNT_FACTOR;
+        $class = null;
+        switch ($type) {
+            case 10:
+                return ProzentSkala::create();
+                break;
+            case 20:
+                return PunktSkala::fromMaxPunktzahl(Punktzahl::fromFloat($amount));
+                break;
+        }
+        throw new \Exception("Skala-Typ aus DB '$type' nicht bekannt!'");
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform) {
+        if (!$value instanceof Skala) {
+            throw new \Exception("Skala-Typ muss Interface 'Skala' implementieren");
+        }
+        if ($value instanceof ProzentSkala) {
+            return self::PROZENT_SKALA;
+        } elseif ($value instanceof PunktSkala) {
+            return self::AMOUNT_FACTOR * $value->getMaxPunktzahl()->getValue() + self::PUNKT_SKALA;
+        }
+        throw new \Exception("Skala-Typ '" . get_class($value) . "'ist unbekannt!");
+    }
+
+    public function getName() {
+        return self::TYPE_NAME;
+    }
+
+    public function requiresSQLCommentHint(AbstractPlatform $platform) {
+        return TRUE;
+    }
+}
