605c34fcb80e1a6cb6ec8b3f0fc7be8bdc7507e5
Martin Dittmar <martin.dittmar@charite.de>
2019-09-16 17:52:11 +0200

    Vereinfachung AutoIncrementTrait

--- src/Common/Infrastructure/Persistence/DB/DoctrineAutoIncrementTrait.php
+++ src/Common/Infrastructure/Persistence/DB/DoctrineAutoIncrementTrait.php
@@ -15,11 +15,10 @@ trait DoctrineAutoIncrementTrait
     protected function getAndIncreaseAutoIncrementOfTable(): int {
         $this->entityManager->beginTransaction();
         $tableName = $this->getTableNameOfRepo();
-        $dbName = $this->entityManager->getConnection()->getDatabase();
 
         // Lesen-Schreiben-Lesen um race conditions zu verhindern.
-        $ai1 = $this->getCurrentAutoIncrement($tableName, $dbName);
-        $this->increaseAutoIncrement($tableName, $dbName);
+        $ai1 = $this->getCurrentAutoIncrement($tableName);
+        $this->increaseAutoIncrement($tableName);
 
 
         $this->entityManager->commit();
@@ -32,11 +31,11 @@ trait DoctrineAutoIncrementTrait
         return $this->entityManager->getClassMetadata($className)->getTableName();
     }
 
-    protected function getCurrentAutoIncrement(string $tableName, string $dbName): int {
+    protected function getCurrentAutoIncrement(string $tableName): int {
         $rsm = new ResultSetMapping();
         $rsm->addScalarResult("AUTO_INCREMENT", "AUTO_INCREMENT", "integer");
         $sql = "SELECT `AUTO_INCREMENT` FROM INFORMATION_SCHEMA.TABLES"
-            . " WHERE TABLE_SCHEMA = '$dbName' AND TABLE_NAME = '$tableName'";
+            . " WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = '$tableName'";
 
         $maxId = $this->entityManager->createNativeQuery($sql, $rsm)->getResult()[0]["AUTO_INCREMENT"];
         $maxId = max([1, $maxId]);
@@ -44,12 +43,11 @@ trait DoctrineAutoIncrementTrait
         return $maxId;
     }
 
-    protected function increaseAutoIncrement(string $tableName, string $dbName): void {
+    protected function increaseAutoIncrement(string $tableName): void {
         $sql =
-            "SET @ai = (SELECT `AUTO_INCREMENT` + 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA  = '$dbName' AND TABLE_NAME = '$tableName');"
+            "SET @ai = (SELECT `AUTO_INCREMENT` + 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA  = DATABASE() AND TABLE_NAME = '$tableName');"
             . "SET @ai = CONCAT('ALTER TABLE $tableName AUTO_INCREMENT = ', @ai);"
             . "PREPARE stmt FROM @ai ;EXECUTE stmt;";
         $this->entityManager->getConnection()->executeUpdate($sql);
-
     }
 }
