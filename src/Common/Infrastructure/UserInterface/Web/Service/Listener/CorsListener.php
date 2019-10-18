ab3369ca8aef370a39374c51ffd4f89e8c6e6fb3
Martin Dittmar <martin.dittmar@charite.de>
2019-10-18 15:58:35 +0200

    Add CORS header für lokale Tests

---
+++ src/Common/Infrastructure/UserInterface/Web/Service/Listener/CorsListener.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Common\Infrastructure\UserInterface\Web\Service\Listener;
+
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+
+class CorsListener
+{
+    public function onKernelResponse(ResponseEvent $event) {
+        $responseHeaders = $event->getResponse()->headers;
+
+        $responseHeaders->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        $responseHeaders->set('Access-Control-Allow-Credentials', "true");
+        $responseHeaders->set('Access-Control-Allow-Headers', 'origin, content-type, accept');
+        $responseHeaders->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
+    }
+}
