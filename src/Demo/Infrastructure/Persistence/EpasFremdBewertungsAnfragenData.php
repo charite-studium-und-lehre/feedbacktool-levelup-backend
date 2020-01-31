41d47141a55c69be35a1f247a007067bc7ef03be
Martin Dittmar <martin.dittmar@charite.de>
2020-01-31 16:51:50 +0100

    EPA-Demo-DAten anpassen an neuen Frontend-Release

---
+++ src/Demo/Infrastructure/Persistence/EpasFremdBewertungsAnfragenData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Demo\Infrastructure\Persistence;
+
+class EpasFremdBewertungsAnfragenData extends AbstractJsonDemoData
+{
+    protected array $jsonData = [
+        "fremdbewertungsAnfragen" => [
+            [
+                "id"                  => 51,
+                "name"                => "March Simpson",
+                "email"               => "mdi@posteo.de",
+                "anfrageTaetigkeiten" => "",
+                "anfrageKommentar"    => "Gelbsucht",
+                "datum"               => "2019-11-13",
+            ],
+        ],
+    ];
+
+    public function isResponsibleFor(string $pathInfo): bool {
+        return $pathInfo == "api/epas/fremdbewertungen/anfragen";
+    }
+}
