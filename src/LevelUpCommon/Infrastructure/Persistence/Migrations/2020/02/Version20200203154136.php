0e1e06b82548292a628797706e62d4ee13a0eb93
Martin Dittmar <martin.dittmar@charite.de>
2020-02-03 16:30:19 +0100

    Fix Exception Fremdbewertung

---
+++ src/LevelUpCommon/Infrastructure/Persistence/Migrations/2020/02/Version20200203154136.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LevelUpCommon\Infrastructure\Persistence\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200203154136 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE pruefung_frage CHANGE id id VARCHAR(30) NOT NULL COMMENT \'(DC2Type:fragenId)\'');
+        $this->addSql('ALTER TABLE pruefung_frage_antwort CHANGE id id VARCHAR(30) NOT NULL COMMENT \'(DC2Type:antwortId)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE pruefung_frage CHANGE id id VARCHAR(30) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:aggregateIdString)\'');
+        $this->addSql('ALTER TABLE pruefung_frage_antwort CHANGE id id VARCHAR(30) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:aggregateIdString)\'');
+    }
+}
---
+++ src/LevelUpCommon/Infrastructure/Persistence/Migrations/2020/02/Version20200203154544.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LevelUpCommon\Infrastructure\Persistence\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200203154544 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE pruefung_frage CHANGE pruefungsItemId pruefungsItemId VARCHAR(30) NOT NULL COMMENT \'(DC2Type:pruefungsItemId)\'');
+        $this->addSql('ALTER TABLE pruefung_frage_antwort CHANGE fragenId fragenId VARCHAR(30) NOT NULL COMMENT \'(DC2Type:fragenId)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE pruefung_frage CHANGE pruefungsItemId pruefungsItemId VARCHAR(30) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:aggregateIdString)\'');
+        $this->addSql('ALTER TABLE pruefung_frage_antwort CHANGE fragenId fragenId VARCHAR(30) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:aggregateIdString)\'');
+    }
+}
